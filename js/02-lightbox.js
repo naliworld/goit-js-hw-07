7f78c1d5d216d35c9c7f1d04b46f387cdb53be87
naliworld <123972576+naliworld@users.noreply.github.com>
2023-06-18 20:42:07 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+
+
+
+
+let lightbox = new SimpleLightbox (".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
 console.log(galleryItems);

88f13a58e4fb474f030eb30983de7925e65338d4
naliworld <123972576+naliworld@users.noreply.github.com>
2023-06-26 20:14:12 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,35 +1,31 @@
 import { GalleryItems } from './gallery-items.js';
 // Change code below this line
 const GalleryImages = document.querySelector("ul.gallery");
-const markup = createImageGallery(GalleryItems)
+const markup = createImageGallery(GalleryItems);
 
-GalleryImages.insertAdjacentElement("beforeend", markup);
+GalleryImages.insertAdjacentHTML("beforeend", markup);
 
 function createImageGallery(GalleryItems) {
     return GalleryItems
     .map( 
         ({preview, original, description}) => {
-            return ` 
-            <li class = "gallery__item">
-            <a class = "gallery__link" href = "${original}">
-            <img class = "gallery__image" src="${preview}" alt = ${description}" />
+            return` 
+            <li class="gallery__item">
+             <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
             </a>
-            </li> ` ;
-        }
-    )
-};
-join("");
-const onGalleryItemsClick = (e) => {
+            </li>`;
+        })
+    .join("");
+}   
+function onGalleryItemsClick(e) {
     e.preventDefault();
-
-    if (e.target.classList.contains("gallery__image")) 
-    return;
+    if (e.target.classList.contains("gallery__image"))
+        return;
     const source = e.target.dataset.source;
-    const instance = basicLightbox.create (`
-     <img src = "${source}" width = "800" height = "600"></img> `), 
-    onShow: (instance) => galleryImages.addEventListener("click", onGalleryItemsClick),
-    onClose: (instance) => galleryImages.removeEventListener ("click", onGalleryItemsClick),`
+    const instance = basicLightbox.create(`<img src="${source}" width="800" height="600"></img>`);
+    onShow;(instance) => galleryImages.addEventListener; ("click", onGalleryItemsClick);
+    onClose;(instance) => galleryImages.removeEventListener("click", onGalleryItemsClick);
 }
-
 galleryImages.addEventListener("click", onGalleryItemsClick);
 console.log(galleryItems);

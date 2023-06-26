281ca9b23b6d1e685af87b48a86182932c39e975
naliworld <123972576+naliworld@users.noreply.github.com>
2023-06-26 20:14:15 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,16 +8,14 @@ GalleryImages.insertAdjacentHTML("beforeend", markup);
 function createImageGallery(galleryItems) {
     return galleryItems
     .map(({preview, original, description}) => {
-        return ` 
+        return`
         <li class = "gallery__item">
-        <a class= "gallery__link" href="${original}">
+        <a class = "gallery__link" href="${original}">
         <img class = "gallery__image" src="${preview}" alt = "${description}" />
         </a>
         </li>`;
-    }
-    )
+    })
 }
-
 let lightbox = new SimpleLightbox (".gallery a", {
     captionsData: "alt",
     captionDelay: 250,

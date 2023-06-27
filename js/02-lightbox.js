5b2ee841e6085acdb2412fa41bffe4167c3d4e8e
naliworld <123972576+naliworld@users.noreply.github.com>
2023-06-27 19:54:33 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const GalleryImages = document.querySelector("ul.gallery");
+const galleryImages = document.querySelector("ul.gallery");
 const markup = createImageGallery(galleryItems);
 
-GalleryImages.insertAdjacentHTML("beforeend", markup);
+galleryImages.insertAdjacentHTML("beforeend", markup);
 
 function createImageGallery(galleryItems) {
     return galleryItems
@@ -11,7 +11,7 @@ function createImageGallery(galleryItems) {
         return`
         <li class = "gallery__item">
         <a class = "gallery__link" href="${original}">
-        <img class = "gallery__image" src="${preview}" alt = "${description}" />
+        <img class = "gallery__image" src="${preview}" alt="${description}" />
         </a>
         </li>`;
     })
@@ -20,4 +20,4 @@ let lightbox = new SimpleLightbox (".gallery a", {
     captionsData: "alt",
     captionDelay: 250,
 });
-console.log(galleryItems);
+console.log(galleryItems); 

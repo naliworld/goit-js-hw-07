82cb2fc7560e68e99fd8afe73272182c484d750c
naliworld <123972576+naliworld@users.noreply.github.com>
2023-06-27 20:18:52 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -18,14 +18,14 @@ function createImageGallery(galleryItems) {
         })
     .join("");
 }   
-function onGalleryItemsClick(e) {
+function ongalleryItemsClick(e) {
     e.preventDefault();
     if (e.target.classList.contains("gallery__image"))
         return;
     const source = e.target.dataset.source;
     const instance = basicLightbox.create(`<img src="${source}" width="800" height="600"></img>`);
-    onShow:(instance) => galleryImages.addEventListener; ("click", onGalleryItemsClick);
-    onClose:(instance) => galleryImages.removeEventListener("click", onGalleryItemsClick);
+    onShow:(instance) => galleryImages.addEventListener; ("click", ongalleryItemsClick);
+    onClose:(instance) => galleryImages.removeEventListener("click", ongalleryItemsClick);
 }
 galleryImages.addEventListener("click", onGalleryItemsClick);
 console.log(galleryItems);
